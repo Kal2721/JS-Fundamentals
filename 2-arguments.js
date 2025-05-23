a65bf19ffc7844871f380c5a6f866f812c30db25
kalkidan <kalkidanteshale6@gmail.com>
2025-05-23 22:46:09 +0300

    A script that prints a message depending of the number of arguments passed

---
+++ 2-arguments.js
@@ -0,0 +1,14 @@
+const argument = function(){
+	switch(argument.length){
+		case 0:
+			console.log('No argument');
+			break;
+		case 1:
+			console.log('Argument found');
+			break;
+		default:
+			console.log('Arguments found');
+	};
+}
+
+argument();
