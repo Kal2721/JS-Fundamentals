50b869d68362613a0c535a6448a316d0a8ed09cc
kalkidan <kalkidanteshale6@gmail.com>
2025-05-25 16:05:48 +0300

    A script that prints a square

--- 8-square.js
+++ 8-square.js
@@ -4,7 +4,7 @@ const printSquare = function(x){
 	Array.from({ length : x }).forEach(() =>{
 		let row = "";
 		for(let i =0; i < x; i++){
-			row += "x";
+			row += "X";
 		}
 			console.log(row);
 	});
